fixed world state savings not being saved in px but in field units

world/world_state.py
@@ -29,14 +29,14 @@ class WorldState:
 
         # add player
         player_list = [self.OBJECT_TYPE_TO_INT[self.world.player.__class__], self.world.player.rect.x, self.world.player.rect.y,
-                       self.world.player.width]
+                       self.world.player.rect.width]
         game_objects.append(player_list)
 
         # add objects
         for lane in self.world.directed_lanes:
             if isinstance(lane, DirectedLane):
                 for obj in lane.non_player_sprites.sprites():
-                    object_list = [self.OBJECT_TYPE_TO_INT[obj.__class__], obj.rect.x, obj.rect.y, obj.width]
+                    object_list = [self.OBJECT_TYPE_TO_INT[obj.__class__], obj.rect.x, obj.rect.y, obj.rect.width]
                     game_objects.append(object_list)
 
         return np.asarray(game_objects)