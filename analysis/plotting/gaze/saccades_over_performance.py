9753ae6226903ba068c152a070c410c83e1e751e
tobnie <niehues.tobias@gmail.com>
2022-08-29 11:45:46 +0200

    filtering for saccades by duration threshold

--- analysis/plotting/gaze/saccades_over_performance.py
+++ analysis/plotting/gaze/saccades_over_performance.py
@@ -3,9 +3,11 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-from analysis.data_utils import read_data, read_subject_data
+from analysis.data_utils import read_data
 from analysis.plotting.gaze.events.event_detection import try_saccade_detection
 from analysis.plotting.gaze.events.saccade_utils import calc_sacc_amplitude, calc_sacc_angle
+
+SACC_DURATION_THRESHOLD = 400
 
 
 def get_saccade_dataframe(df):
@@ -33,65 +35,74 @@ def get_saccade_dataframe(df):
 
     sacc_df['amplitude'] = sacc_df[['x_start', 'y_start', 'x_end', 'y_end']].apply(lambda x: calc_sacc_amplitude(*x.values), axis=1)
     sacc_df['angle'] = sacc_df[['x_start', 'y_start', 'x_end', 'y_end']].apply(lambda x: calc_sacc_angle(*x.values), axis=1)
+    sacc_df['duration'] = sacc_info_df['duration']
     return sacc_df
 
 
-def plot_sacc_per_subject_experience(df):
+def filter_saccade_df(df):
+    """ Filters the given dataframe by dropping saccades with a too long duration"""
+    print('Saccades before Filtering:', len(df))
+    filtered_df = df[df['duration'] <= SACC_DURATION_THRESHOLD]
+    print('Saccades after Filtering:', len(filtered_df))
+    return filtered_df
+
+
+def plot_sacc_per_subject_experience(df, directory='./imgs/saccades/'):
     fig, ax = plt.subplots()
     sns.barplot(data=df, x='experience', y='amplitude', ax=ax)
 
-    plt.savefig('./imgs/saccades/amplitudes_per_experience.png')
+    plt.savefig(directory + 'amplitudes_per_experience.png')
     plt.show()
 
     sns.barplot(data=df, x='experience', y='angle')
-    plt.savefig('./imgs/saccades/angles_per_experience.png')
+    plt.savefig(directory + 'angles_per_experience.png')
     plt.show()
 
 
-def plot_sacc_per_subject_score(df):
+def plot_sacc_per_subject_score(df, directory='./imgs/saccades/'):
     sns.barplot(data=df, y='amplitude', x='score', hue='experience')
     plt.title('the more red, the more experienced the player')
-    plt.savefig('./imgs/saccades/amplitudes_per_score.png')
+    plt.savefig(directory +'amplitudes_per_score.png')
     plt.show()
 
     sns.barplot(data=df, y='angle', x='score', hue='experience')
     plt.title('the more red, the more experienced the player')
-    plt.savefig('./imgs/saccades/angles_per_score.png')
+    plt.savefig(directory + 'angles_per_score.png')
     plt.show()
 
 
-def plot_sacc_per_trial(df):
+def plot_sacc_per_trial(df, directory='./imgs/saccades/'):
     # over all subjects
     sns.lineplot(data=df, y='amplitude', x='trial')
     plt.title('Sacc Amplitude per Trial')
-    plt.savefig('./imgs/saccades/amplitudes_per_trial.png')
+    plt.savefig(directory + 'amplitudes_per_trial.png')
     plt.show()
 
     sns.lineplot(data=df, y='angle', x='trial')
     plt.title('Sacc Angle per Trial')
-    plt.savefig('./imgs/saccades/angles_per_trial.png')
+    plt.savefig(directory + 'angles_per_trial.png')
     plt.show()
 
     # per experience
     sns.lineplot(data=df, y='amplitude', x='trial', hue='experience')
     plt.title('Sacc Amplitude per Trial')
-    plt.savefig('./imgs/saccades/amplitudes_per_trial_per_experience.png')
+    plt.savefig(directory + 'amplitudes_per_trial_per_experience.png')
     plt.show()
 
     sns.lineplot(data=df, y='angle', x='trial', hue='experience')
     plt.title('Sacc Angle per Trial')
-    plt.savefig('./imgs/saccades/angles_per_trial_per_experience.png')
+    plt.savefig(directory + 'angles_per_trial_per_experience.png')
     plt.show()
 
     # per subject
     sns.lineplot(data=df, y='amplitude', x='trial', hue='subject_id')
     plt.title('Sacc Amplitude per Trial')
-    plt.savefig('./imgs/saccades/amplitudes_per_trial_per_subject.png')
+    plt.savefig(directory + 'amplitudes_per_trial_per_subject.png')
     plt.show()
 
     sns.lineplot(data=df, y='angle', x='trial', hue='subject_id')
     plt.title('Sacc Angle per Trial')
-    plt.savefig('./imgs/saccades/angles_per_trial_per_subject.png')
+    plt.savefig(directory + 'angles_per_trial_per_subject.png')
     plt.show()
 
     # per score
@@ -99,27 +110,27 @@ def plot_sacc_per_trial(df):
     g.map_dataframe(sns.barplot, x='trial', y='amplitude', ci=None)
     plt.suptitle('Sacc Amplitude per Trial')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitudes_per_trial_per_score.png')
+    plt.savefig(directory + 'amplitudes_per_trial_per_score.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="score", margin_titles=True)
     g.map_dataframe(sns.barplot, x='trial', y='angle', ci=None)
     plt.suptitle('Sacc Angle per Trial')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angles_per_trial_per_score.png')
+    plt.savefig(directory + 'angles_per_trial_per_score.png')
     plt.show()
 
 
-def plot_sacc_per_difficulty(df):
+def plot_sacc_per_difficulty(df, directory='./imgs/saccades/'):
     # over all subjects per difficulty
     sns.barplot(data=df, x='game_difficulty', y='amplitude')
     plt.title('Sacc Amplitude per Trial')
-    plt.savefig('./imgs/saccades/amplitudes_per_difficulty.png')
+    plt.savefig(directory + 'amplitudes_per_difficulty.png')
     plt.show()
 
     sns.barplot(data=df, x='game_difficulty', y='angle')
     plt.title('Sacc Angle per Trial')
-    plt.savefig('./imgs/saccades/angles_per_difficulty.png')
+    plt.savefig(directory + 'angles_per_difficulty.png')
     plt.show()
 
     # over time per world
@@ -127,14 +138,14 @@ def plot_sacc_per_difficulty(df):
     g.map_dataframe(sns.histplot, x='time', y='amplitude')
     plt.suptitle('Sacc Amplitude over time per world')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_over_time_per_world.png')
+    plt.savefig(directory + 'amplitude_over_time_per_world.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="world_number", row="game_difficulty", margin_titles=True)
     g.map_dataframe(sns.histplot, x='time', y='angle')
     plt.suptitle('Sacc Angle over time per world')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_over_time_per_world.png')
+    plt.savefig(directory + 'angle_over_time_per_world.png')
     plt.show()
 
     # barplot over world
@@ -142,27 +153,27 @@ def plot_sacc_per_difficulty(df):
     g.map_dataframe(sns.barplot, x='world_number', y='amplitude', ci=None)
     plt.suptitle('Sacc Amplitude per world and difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_per_world_and_difficulty.png')
+    plt.savefig(directory + 'amplitude_per_world_and_difficulty.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="game_difficulty", margin_titles=True)
     g.map_dataframe(sns.barplot, x='world_number', y='angle', ci=None)
     plt.suptitle('Sacc Angle per world and difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_per_world_and_difficulty.png')
+    plt.savefig(directory + 'angle_per_world_and_difficulty.png')
     plt.show()
 
 
-def plot_sacc_per_game_status(df):
+def plot_sacc_per_game_status(df, directory='./imgs/saccades/'):
     # over all subjects per difficulty
     sns.barplot(data=df, x='game_status', y='amplitude')
     plt.title('Sacc Amplitude per game outcome')
-    plt.savefig('./imgs/saccades/amplitudes_per_game_outcome.png')
+    plt.savefig(directory + 'amplitudes_per_game_outcome.png')
     plt.show()
 
     sns.barplot(data=df, x='game_status', y='angle')
     plt.title('Sacc Angle per game outcome')
-    plt.savefig('./imgs/saccades/angles_per_game_outcome.png')
+    plt.savefig(directory + 'angles_per_game_outcome.png')
     plt.show()
 
     # over time per world
@@ -170,31 +181,31 @@ def plot_sacc_per_game_status(df):
     g.map_dataframe(sns.histplot, x='time', y='amplitude')
     plt.suptitle('Sacc Amplitude over time per game outcome')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_over_time_per_game_outcome.png')
+    plt.savefig(directory + 'amplitude_over_time_per_game_outcome.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="game_status", margin_titles=True)
     g.map_dataframe(sns.histplot, x='time', y='angle')
     plt.suptitle('Sacc Angle over time per game outcome')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_over_time_per_game_outcome.png')
+    plt.savefig(directory + 'angle_over_time_per_game_outcome.png')
     plt.show()
 
 
-def plot_sacc_histograms(df):
+def plot_sacc_histograms(df,  directory='./imgs/saccades/'):
     # over game outcome
     g = sns.FacetGrid(df, col="game_status", margin_titles=True)
     g.map(sns.histplot, 'amplitude', stat='density')
     plt.suptitle('Sacc Amplitudes per game outcome')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_hist_per_game_outcome.png')
+    plt.savefig(directory + 'amplitude_hist_per_game_outcome.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="game_status", margin_titles=True)
     g.map(sns.histplot, 'angle', stat='density')
     plt.suptitle('Sacc Angles per game outcome')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_hist_per_game_outcome.png')
+    plt.savefig(directory + 'angle_hist_per_game_outcome.png')
     plt.show()
 
     # over experience
@@ -202,14 +213,14 @@ def plot_sacc_histograms(df):
     g.map_dataframe(sns.histplot, 'amplitude', stat='density')
     plt.suptitle('Sacc Amplitudes per experience')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_hist_per_experience.png')
+    plt.savefig(directory + 'amplitude_hist_per_experience.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="experience", margin_titles=True)
     g.map_dataframe(sns.histplot, 'angle', stat='density')
     plt.suptitle('Sacc Angles per experience')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_hist_per_experience.png')
+    plt.savefig(directory + 'angle_hist_per_experience.png')
     plt.show()
 
     # over difficulty
@@ -217,14 +228,14 @@ def plot_sacc_histograms(df):
     g.map_dataframe(sns.histplot, 'amplitude', stat='density')
     plt.suptitle('Sacc Amplitudes per difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_hist_per_difficulty.png')
+    plt.savefig(directory + 'amplitude_hist_per_difficulty.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="game_difficulty", margin_titles=True)
     g.map_dataframe(sns.histplot, 'angle', stat='density')
     plt.suptitle('Sacc Angles per difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_hist_per_difficulty.png')
+    plt.savefig(directory + 'angle_hist_per_difficulty.png')
     plt.show()
 
     # over score
@@ -232,14 +243,14 @@ def plot_sacc_histograms(df):
     g.map_dataframe(sns.histplot, 'amplitude', stat='density')
     plt.suptitle('Sacc Amplitudes per Score')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_hist_per_score.png')
+    plt.savefig(directory + 'amplitude_hist_per_score.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="score", margin_titles=True)
     g.map_dataframe(sns.histplot, 'angle', stat='density')
     plt.suptitle('Sacc Angles per Score')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_hist_per_score.png')
+    plt.savefig(directory + 'angle_hist_per_score.png')
     plt.show()
 
     # over difficulty
@@ -247,14 +258,14 @@ def plot_sacc_histograms(df):
     g.map_dataframe(sns.histplot, 'amplitude', stat='density')
     plt.suptitle('Sacc Amplitudes per difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/amplitude_hist_per_difficulty_per_world.png')
+    plt.savefig(directory + 'amplitude_hist_per_difficulty_per_world.png')
     plt.show()
 
     g = sns.FacetGrid(df, col="world_number", row='game_difficulty', margin_titles=True)
     g.map_dataframe(sns.histplot, 'angle', stat='density')
     plt.suptitle('Sacc Angles per difficulty')
     plt.tight_layout()
-    plt.savefig('./imgs/saccades/angle_hist_per_difficulty_per_world.png')
+    plt.savefig(directory + 'angle_hist_per_difficulty_per_world.png')
     plt.show()
 
 
@@ -267,19 +278,24 @@ def plot_sacc_histograms(df):
 df = read_data()
 # df = read_subject_data('AN06AN')
 
-# drop rows with no trial information:
-df_without_nan_trials = df.dropna()
 
 # get saccade data
 sacc_df = get_saccade_dataframe(df)
-sacc_df_without_nan_trials = get_saccade_dataframe(df_without_nan_trials)
+
+# threshold saccades
+sacc_df = filter_saccade_df(sacc_df)
 
 # plot
-plot_sacc_histograms(sacc_df)
-plot_sacc_per_game_status(sacc_df)
-plot_sacc_per_difficulty(sacc_df)
-plot_sacc_per_subject_score(sacc_df)
-plot_sacc_per_subject_experience(sacc_df)
+plot_sacc_histograms(sacc_df, directory='./imgs/saccades/filtered_saccades/')
+plot_sacc_per_game_status(sacc_df, directory='./imgs/saccades/filtered_saccades/')
+plot_sacc_per_difficulty(sacc_df, directory='./imgs/saccades/filtered_saccades/')
+plot_sacc_per_subject_score(sacc_df, directory='./imgs/saccades/filtered_saccades/')
+plot_sacc_per_subject_experience(sacc_df, directory='./imgs/saccades/filtered_saccades/')
 
 # need to drop nan trials for next plot:
-plot_sacc_per_trial(sacc_df_without_nan_trials)
+# drop rows with no trial information:
+df_without_nan_trials = df.dropna()
+sacc_df_without_nan_trials = get_saccade_dataframe(df_without_nan_trials)
+sacc_df_without_nan_trials = filter_saccade_df(sacc_df_without_nan_trials)
+
+plot_sacc_per_trial(sacc_df_without_nan_trials, directory='./imgs/saccades/filtered_saccades/')
