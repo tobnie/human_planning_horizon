98f7c8f1beffe6ff03522ea9b58c916cdca97fca
tobnie <niehues.tobias@gmail.com>
2022-08-29 09:46:16 +0200

    fixed position data not being saved

--- neural_network/save_pos_data.py
+++ neural_network/save_pos_data.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-from analysis.data_utils import read_data
+from analysis.data_utils import read_data, get_only_onscreen_data
 
 df = read_data()
 df = df[['player_x_field', 'player_y_field', 'gaze_x', 'gaze_y']]
 df.dropna(inplace=True)
 
+df = get_only_onscreen_data(df)
+
 # get data in arrays
 player_pos = df[['player_x_field', 'player_y_field']].to_numpy()
 
 # save data
-np.savez_compressed('input_position.npz')
+np.savez_compressed('input_position.npz', player_pos)
