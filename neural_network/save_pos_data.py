923cbe1f73572b3f696b533cbb01b172832a215f
tobnie <niehues.tobias@gmail.com>
2022-08-29 09:30:23 +0200

    discrete player positions as input for NN

---
+++ neural_network/save_pos_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from analysis.data_utils import read_data
+
+df = read_data()
+df = df[['player_x_field', 'player_y_field', 'gaze_x', 'gaze_y']]
+df.dropna(inplace=True)
+
+# get data in arrays
+player_pos = df[['player_x_field', 'player_y_field']].to_numpy()
+
+# save data
+np.savez_compressed('input_position.npz')
