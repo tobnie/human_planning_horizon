9bd06f784a4a5800b10494c6d19d7f21075bfe42
tobnie <niehues.tobias@gmail.com>
2022-09-15 17:33:33 +0200

    removed double save of blinks in preprocessing

--- data/run_preprocessing.py
+++ data/run_preprocessing.py
@@ -1,15 +1,17 @@
-from analysis.gaze.blinks import save_blinks
-from analysis.gaze.fixations import save_fixation_info
+from analysis.gaze.blink_rates import save_blink_rates
+from analysis.gaze.blinks import save_blinks, save_blinks_and_IBIs
+from analysis.gaze.fixations import save_fixations
 from analysis.gaze.saccades import save_saccades
 from analysis.performance.performances import save_performance_stats, save_game_durations, save_level_scores
 from data.preprocessing import run_preprocessing
 
-# run_preprocessing()
+run_preprocessing()
 
 # create separate dataframes for faster loading
-save_blinks()
-save_fixation_info()
-# save_saccades()
-# save_performance_stats()
-# save_game_durations()
-# save_level_scores()
+save_blinks_and_IBIs()
+save_blink_rates()
+save_fixations()
+save_saccades()
+save_performance_stats()
+save_game_durations()
+save_level_scores()
